ada667e905b698761defa01caea183d1e4e20cf1
hamed18 <hamedcnec@gmail.com>
2024-09-17 12:38:42 +0600

    Patch Update in modal. Filter data in Requested asset page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -27,6 +27,7 @@ async function run() {
 
     // CREATE: send user input data from server to database. client: Join As HR
     const UsersCollection = client.db("Assets").collection("Users");
+    const AssetsCollection = client.db("Assets").collection("AllAssets");
     app.post("/addUser", async (req, res) => {
       const newUser = req.body;
       console.log(newUser);
@@ -34,12 +35,12 @@ async function run() {
       res.send(result);
     });
 
-    // // READ: get data from db. 
-    // app.get('/users', async(req,res) => {
-    //   const cursor = UsersCollection.find();
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // })
+    // READ: get data from db. 
+    app.get('/users', async(req,res) => {
+      const cursor = UsersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     // load data by email 
     app.get("/users/:email", async (req, res) => {
@@ -50,6 +51,28 @@ async function run() {
       res.json(result);
       console.log("email query",result);
     });
+
+     // READ: get data from db. 
+     app.get('/allAssets', async(req,res) => {
+      const cursor = AssetsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    //
+    app.patch('/allAssets/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };  // filter select the particular document that needs to be updated
+      const updatedAsset = req.body;
+      console.log(updatedAsset);
+      const updateDoc = {
+        $set: {
+          Status : updatedAsset.Status
+        }
+      };
+      const result = await AssetsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
